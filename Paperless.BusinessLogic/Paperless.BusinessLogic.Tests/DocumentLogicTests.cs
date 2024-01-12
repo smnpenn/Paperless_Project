2b7e185cf119b474f10ccecc1d9b11f999b55c58
Simse <smnpenn@gmail.com>
2024-01-12 11:19:07 +0100

    wip unit testing

---
+++ Paperless.BusinessLogic/Paperless.BusinessLogic.Tests/DocumentLogicTests.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Moq;
+using Moq.Protected;
+using Paperless.BusinessLogic.Interfaces;
+using Paperless.BusinessLogic.Entities;
+using AutoFixture;
+using System.Runtime.CompilerServices;
+using Paperless.DAL.Interfaces;
+using AutoMapper;
+using Paperless.ServiceAgents.Interfaces;
+using Nest;
+
+namespace Paperless.BusinessLogic.Tests
+{
+    public class DocumentLogicTests
+    {
+        private Fixture _fixture;
+        private Mock<IDocumentRepository> _repo;
+        private Mock<IMapper> _mapper;
+        private Mock<IRabbitMQService> _rabbitmqService;
+        private Mock<IMinIOServiceAgent> _minioService;
+        private Mock<IElasticSearchServiceAgent> _elasticSearchServiceAgent;
+        private DocumentLogic documentLogic;
+
+        public DocumentLogicTests() 
+        {
+            _fixture = new Fixture();
+            _repo = new Mock<IDocumentRepository>();
+            _mapper = new Mock<IMapper>();
+            _rabbitmqService = new Mock<IRabbitMQService>();
+            _minioService = new Mock<IMinIOServiceAgent>();
+            _elasticSearchServiceAgent = new Mock<IElasticSearchServiceAgent>();
+            documentLogic = new DocumentLogic(_repo.Object, _mapper.Object, _rabbitmqService.Object, _minioService.Object, _elasticSearchServiceAgent.Object);
+        } 
+
+        [Test]
+        public void GetDocumentById_Test_Valid()
+        {
+            DAL.Entities.Document doc = _fixture.Create<DAL.Entities.Document>();
+            Document blDoc = new Document
+            {
+                Title = doc.Title,
+                Id = doc.Id,
+                Added = doc.Added,
+                Created = doc.Created,
+                Correspondent = doc.Correspondent,
+                Modified = doc.Modified,
+                Content = doc.Content,
+                Path = doc.Path,
+                DocumentType = doc.DocumentType,
+                Tags = doc.Tags,
+            };
+            _repo.Setup(r => r.GetDocumentById(doc.Id)).Returns(doc);
+            _mapper.Setup(m => m.Map<DAL.Entities.Document, Document>(_repo.Object.GetDocumentById(doc.Id))).Returns(blDoc);
+
+            var res = documentLogic.GetDocumentById(doc.Id);
+
+            Assert.That(res.Title, Is.EqualTo(doc.Title));    
+        }
+
+        [Test]
+        public void GetDocumentById_Test_Invalid()
+        {
+            DAL.Entities.Document doc = _fixture.Create<DAL.Entities.Document>();
+            _repo.Setup(r => r.GetDocumentById(doc.Id)).Returns((Paperless.DAL.Entities.Document)null);
+            _mapper.Setup(m => m.Map<DAL.Entities.Document, Document>(_repo.Object.GetDocumentById(doc.Id))).Returns((Document)null);
+
+            var res = documentLogic.GetDocumentById(doc.Id);
+
+            Assert.IsNull(res);
+        }
+
+    }
+}
---
+++ Paperless.BusinessLogic/Paperless.BusinessLogic.Tests/DocumentTypeLogicTests.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoFixture;
+using AutoMapper;
+using Moq;
+using Moq.Protected;
+using Paperless.BusinessLogic.Entities;
+using Paperless.DAL.Interfaces;
+
+namespace Paperless.BusinessLogic.Tests
+{
+    public class DocumentTypeLogicTests
+    {
+        private Fixture _fixture;
+        private Mock<IDocumentTypeRepository> _repo;
+        private Mock<IMapper> _mapper;
+        private DocumentTypeLogic _typeLogic;
+
+        public DocumentTypeLogicTests()
+        {
+            _fixture = new Fixture();
+            _repo = new Mock<IDocumentTypeRepository>();
+            _mapper = new Mock<IMapper>();
+            _typeLogic = new DocumentTypeLogic(_repo.Object, _mapper.Object);
+        }
+
+        [Test]
+        public void CreateType_Test_Valid()
+        {
+            DocumentType type = new DocumentType 
+            { 
+                Match = "test",
+                MatchingAlgorithm = 200
+            };
+
+            int res = _typeLogic.CreateType(type);
+
+            Assert.That(res, Is.EqualTo(-1));
+        }
+
+        [Test]
+        public void CreateType_Test_Invalid()
+        {
+            DocumentType type = _fixture.Create<DocumentType>();
+
+            int res = _typeLogic.CreateType(type);
+
+            Assert.That(res, Is.EqualTo(0));
+        }
+    }
+}
